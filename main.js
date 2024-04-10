c29fc821191e2429f7af071d432aa65a99c21bc5
fabianNav <fn39001@gmail.com>
2024-04-10 18:56:37 -0300

    Empezamos a armar la funcion de calcular expensas

--- main.js
+++ main.js
@@ -1,1 +1,30 @@
 import './css/style.css'
+
+let vacationCalc = document.getElementById("vacacionCalc");
+
+vacationCalc.addEventListener('submit', calcExpens);
+
+//TODO Esta funcion calcula las expensas
+function calcExpens(e) {
+  e.preventDefault();
+
+  //? Destino
+  let destiny = document.getElementById('destino').value; 
+  console.log(destiny);
+
+  //? Presupuesto
+  let budget = document.getElementById('presupuesto').value;
+  console.log(budget);
+
+  //? Alojamiento
+  let acomodation = document.getElementById('alojamiento').value;
+  console.log(acomodation);
+
+  //? Transporte
+  let transport = document.getElementById('transporte').value;
+  console.log(transport);
+
+  //? Comida
+  let food = document.getElementById('comida').value;
+  console.log(food);
+}
